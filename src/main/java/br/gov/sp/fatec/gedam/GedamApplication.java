5920e062d936cd00de836fe5f74a80b502488af2
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2021-06-06 21:01:53 -0300

    Inicial do projeto

---
+++ src/main/java/br/gov/sp/fatec/gedam/GedamApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.gedam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/gedam/GedamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.gedam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
